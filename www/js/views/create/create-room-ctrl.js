32e136f0df66c6cb5261dd4831cd2312128afef4
Edison Yang <edison.cy.yang@gmail.com>
2016-07-12 23:17:25 -0700

    Added Create Room form

--- www/js/views/create/create-room-ctrl.js
+++ www/js/views/create/create-room-ctrl.js
@@ -1,12 +1,32 @@
 angular.module('mymessenger.controllers')
 
 
-.controller('CreateRoomCtrl', function($scope, $ionicHistory) {
+.controller('CreateRoomCtrl', function($scope, $ionicHistory, Rooms) {
   console.log("CreateRoom Controller initialized");
    
 
 $scope.goBack = function() {
     $ionicHistory.goBack();
-}
+};
+
+
+  /**
+   * Create a new chat room
+   * 
+   * */
+  $scope.createChatRoom = function(roomName, description) {
+
+      var chatRoom = {
+        chats: {},
+        icon: "ion-university",
+        id: 0,
+        name: roomName,
+        notes: description
+      };
+
+      Rooms.createRoom(chatRoom);
+
+      $ionicHistory.goBack();
+  } 
 
 });
