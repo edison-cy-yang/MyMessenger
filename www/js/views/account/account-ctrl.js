e9c13c3b4a4fe415bdf300b2b9379e9eb6d86c5b
Edison Yang <edison.cy.yang@gmail.com>
2016-07-12 23:56:10 -0700

    Implemented logout

--- www/js/views/account/account-ctrl.js
+++ www/js/views/account/account-ctrl.js
@@ -1,5 +1,13 @@
 angular.module('mymessenger.controllers')
 
-.controller('AccountCtrl', function($scope) {
+.controller('AccountCtrl', function($scope, $state) {
 console.log("AccountCtrl initialized!");
+
+var ref = new Firebase(FIREBASE_URL);
+
+$scope.logout = function() {
+    console.log("logout function called");
+    ref.unauth();
+    $state.go('login');
+}
 });
