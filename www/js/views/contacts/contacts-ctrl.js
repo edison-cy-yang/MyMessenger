e3e975dcc966cd6de39bc652090f207017b5a952
Edison Yang <edison.cy.yang@gmail.com>
2016-07-18 01:12:03 -0700

    Added nav buttons to go to add friends page

--- www/js/views/contacts/contacts-ctrl.js
+++ www/js/views/contacts/contacts-ctrl.js
@@ -1,6 +1,11 @@
 angular.module('mymessenger.controllers')
 
-.controller('ContactsCtrl', function($scope, Friends) {
+.controller('ContactsCtrl', function($scope, Friends, $state) {
     console.log("ContactsCtrl initialized!");
 
+
+$scope.navAddFriend = function() {
+    $state.go("addFriend");
+
+}
 });
