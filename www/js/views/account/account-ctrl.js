833e9877f36d152c39af32b49c2023e7c10a177d
Edison Yang <edison.cy.yang@gmail.com>
2016-07-22 03:48:42 -0700

    Can take profile picture and display in Account tab

--- www/js/views/account/account-ctrl.js
+++ www/js/views/account/account-ctrl.js
@@ -1,19 +1,54 @@
 angular.module('mymessenger.controllers')
 
-.controller('AccountCtrl', function($scope, $state, $cordovaCamera, $firebaseArray) {
+.controller('AccountCtrl', function($scope, $state, $cordovaCamera, $firebaseArray, Account) {
     console.log("AccountCtrl initialized!");
 
 
     var ref = new Firebase(FIREBASE_URL);
-    var syncImages = $firebaseArray(ref.child("images"));
-    $scope.images = [];
-
-    $scope.images = syncImages;
     
 
+
+    /**
+     * Log out of the app
+     */
     $scope.logout = function() {
         console.log("logout function called");
         ref.unauth();
         $state.go('login');
+    };
+
+
+    /**
+     * Take profile picture and save it to user
+     */
+    $scope.takeProfilePic = function() {
+        var options = {
+                quality : 100,
+                destinationType : Camera.DestinationType.DATA_URL,
+                sourceType : Camera.PictureSourceType.CAMERA,
+                allowEdit : true,
+                encodingType: Camera.EncodingType.JPEG,
+                popoverOptions: CameraPopoverOptions,
+                targetWidth: 500,
+                targetHeight: 500,
+                saveToPhotoAlbum: false
+            };
+            $cordovaCamera.getPicture(options).then(function(imageData) {
+            //    Chats.sendImage($scope.displayName, imageData);
+                Account.setProfilePic(imageData);
+
+            }, function(error) {
+               console.error(error);
+            });
+    }
+
+    /**
+     * Load profile pic from firebase to display
+     */
+    $scope.loadProfilePic = function() {
+         Account.getProfilePic(function (profilePic) {
+            $scope.profilePic = profilePic;
+            // console.log($scope.profilePic);
+         });
     }
 });
